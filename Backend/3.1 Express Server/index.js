178a68558ff57febb73e817c062954cd3d3b3533
Olumide Olubosede <olumideolubosede@gmail.com>
2023-11-21 17:05:14 +0100

    Middleware intro

--- Backend/3.1 Express Server/index.js
+++ Backend/3.1 Express Server/index.js
@@ -4,9 +4,25 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  console.log(req);
-  console.log(res);
-  res.send(new URL("../../Frontend/'7.0 CSS Cascade/index.html'"));
+  res.send(
+    "<h1>Hello world</h1><p>Long Strings make the world a better place</p>"
+  );
+});
+
+app.post("/register", (req, res) => {
+  res.sendStatus(201);
+});
+
+app.put("/user/olumide", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.patch("/user/olumide", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.delete("/user/olumide", (req, res) => {
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
