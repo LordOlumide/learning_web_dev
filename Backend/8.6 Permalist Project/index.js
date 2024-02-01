f813526ad672515a335e4b72a867b36de9841e43
Olumide Olubosede <olumideolubosede@gmail.com>
2024-02-01 19:19:12 +0100

    Done with Postgres

--- Backend/8.6 Permalist Project/index.js
+++ Backend/8.6 Permalist Project/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
@@ -7,27 +8,68 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist_project",
+  password: "KingBoss",
+  port: 5432,
+});
+db.connect();
 
-app.get("/", (req, res) => {
+async function getItemsFromDB() {
+  let results = await db.query("SELECT * FROM todos");
+  let items = [];
+  results.rows.forEach((row) => {
+    items.push({id: row['id'], title: row["item"]});
+  });
+  return items;
+}
+
+app.get("/", async (req, res) => {
+let items = [];
+  try {
+    items = await getItemsFromDB();
+  } catch (error) {
+    // fail silently
+  }
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
   });
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
+  console.log(item);
+  try {
+    await db.query("INSERT INTO todos (item) VALUES ($1)", [item]);
+  } catch (error) {
+    // fail silently
+  }
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async (req, res) => {
+  const item = req.body.updatedItemTitle;
+  const id = req.body.updatedItemId;
+  try {
+    await db.query("UPDATE todos SET item = $1 WHERE id = $2", [item, id]);
+  } catch (error) {
+    // fail silently
+  }
+  res.redirect('/');
+});
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", async (req, res) => {
+  const id = req.body.deleteItemId;
+  try {
+    await db.query("DELETE FROM todos WHERE id = $1", [id]);
+  } catch (error) {
+    // fail silently
+  }
+  res.redirect('/');
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
